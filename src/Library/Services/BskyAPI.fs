54eba1fdaa57a8b6f656cd8ac26c99b8615daed8
Angel D. Munoz <scythes.lol@gmail.com>
2024-11-16 08:31:11 -0600

    feat: start adding models and decoders for bluesky feeds

--- src/Library/Services/BskyAPI.fs
+++ src/Library/Services/BskyAPI.fs
@@ -12,51 +12,54 @@ open Flurl
 open Flurl.Http
 open FsToolkit.ErrorHandling
 
-type BskyStrongRef = { cid: string; uri: string }
+open Library
 
-type BskyReplyRef = {
-  root: BskyStrongRef
-  parent: BskyStrongRef
-}
+module JetstreamTypes =
+  type BskyStrongRef = { cid: string; uri: string }
 
-// partial post, we don't need all the fields
-type BskFeedPost = {
-  text: string
-  createdAt: DateTimeOffset
-  reply: BskyReplyRef option
-  tags: string list option
-}
+  type BskyReplyRef = {
+    root: BskyStrongRef
+    parent: BskyStrongRef
+  }
 
-type Commit = {
-  rev: string
-  operation: string
-  collection: string
-  rkey: string
-  record: BskFeedPost option
-  cid: string
-}
+  // partial post, we don't need all the fields
+  type Post = {
+    text: string
+    createdAt: DateTimeOffset
+    reply: BskyReplyRef option
+    tags: string list option
+  }
 
-type Identity = {
-  did: string
-  handle: string
-  seq: int64
-  time: string
-}
+  type Commit = {
+    rev: string
+    operation: string
+    collection: string
+    rkey: string
+    record: Post option
+    cid: string
+  }
 
-type Account = {
-  active: bool
-  did: string
-  seq: int64
-  time: string
-}
+  type Identity = {
+    did: string
+    handle: string
+    seq: int64
+    time: string
+  }
 
-type Event = {
-  did: string
-  time_us: int64
-  commit: Commit option
-  account: Account option
-  identity: Identity option
-}
+  type Account = {
+    active: bool
+    did: string
+    seq: int64
+    time: string
+  }
+
+  type Event = {
+    did: string
+    time_us: int64
+    commit: Commit option
+    account: Account option
+    identity: Identity option
+  }
 
 type Profile = {
   did: string
@@ -71,13 +74,83 @@ type Profile = {
   createdAt: DateTimeOffset
 }
 
-module JsonNode =
-  let inline tryGetProperty (name: string) (json: JsonObject) =
-    match json.TryGetPropertyValue name with
-    | true, value -> ValueSome value
-    | _ -> ValueNone
+module AppBskyRichTextFacet =
+
+   type FacetIndex = { byteEnd: int; byteStart: int }
+   type FacetFeature = {
+     ``$type``: string
+     did: string
+   }
+   type Mention = {
+     ``$type``: string
+     index: FacetIndex
+     features: FacetFeature seq
+   }
+
+module AppBskyFeedGetFeed =
+
+  module BlessedAtUriFeeds =
+      [<Literal>]
+      let WhatsHot = "at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot"
+      [<Literal>]
+      let WhatsHotClassic = "at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/hot-classic"
+      [<Literal>]
+      let BskyTeam = "at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/bsky-team"
+
+  type PostAuthor = {
+    did: string
+    handle: string
+    displayName: string
+    avatar: Uri
+    createdAt: DateTimeOffset
+  }
+
+  module PostAuthor =
+
+     let ofJsonNode (node: JsonNode) = validation {
+       let doc = node.AsObject()
+
+       let! did = JsonNode.tryGetProperty<string> "did" doc
+       and! handle = JsonNode.tryGetProperty<string> "handle" doc
+       and! displayName = JsonNode.tryGetProperty<string> "displayName" doc
+       and! avatar = JsonNode.tryGetProperty<string> "avatar" doc |> Result.map Uri
+
+       let! createdAt = JsonNode.tryGetProperty<string> "createdAt" doc |> Result.map DateTimeOffset.tryOfString
+       let! createdAt = createdAt |> Result.requireValueSome "Unable to convert createdAt"
+
+       return {
+         did = did
+         handle = handle
+         displayName = displayName
+         avatar = avatar
+         createdAt = createdAt
+       }
+     }
 
 
+  type PostRecord = {
+    ``$type``: string
+    createdAt: DateTimeOffset
+    langs: string seq
+    text: string
+  }
+
+  type Post = {
+    uri: Uri
+    cid: string
+    author: PostAuthor
+    record: PostRecord
+    replyCount: int
+    repostCount: int
+    quoteCount: int
+    indexedAt: DateTimeOffset
+    feedContext: string
+  }
+
+  type Feed = {
+    feed: Post seq
+    cursor: string
+  }
 
 module Profile =
 
@@ -85,51 +158,51 @@ module Profile =
     let profile = json.AsObject()
 
     let! did =
-      profile |> JsonNode.tryGetProperty "did" |> ValueOption.map(_.GetValue())
+      profile |> JsonNode.getOptJsonObject "did" |> ValueOption.map(_.GetValue())
 
     let! handle =
       profile
-      |> JsonNode.tryGetProperty "handle"
+      |> JsonNode.getOptJsonObject "handle"
       |> ValueOption.map(_.GetValue())
 
     let! displayName =
       profile
-      |> JsonNode.tryGetProperty "displayName"
+      |> JsonNode.getOptJsonObject "displayName"
       |> ValueOption.map(_.GetValue())
 
     let description =
       profile
-      |> JsonNode.tryGetProperty "description"
+      |> JsonNode.getOptJsonObject "description"
       |> ValueOption.map(_.GetValue())
 
     let avatar =
       profile
-      |> JsonNode.tryGetProperty "avatar"
+      |> JsonNode.getOptJsonObject "avatar"
       |> ValueOption.map(fun v -> v.GetValue() |> Uri)
 
     let banner =
       profile
-      |> JsonNode.tryGetProperty "banner"
+      |> JsonNode.getOptJsonObject "banner"
       |> ValueOption.map(fun v -> v.GetValue() |> Uri)
 
     let! followersCount =
       profile
-      |> JsonNode.tryGetProperty "followersCount"
+      |> JsonNode.getOptJsonObject "followersCount"
       |> ValueOption.map(_.GetValue())
 
     let! followsCount =
       profile
-      |> JsonNode.tryGetProperty "followsCount"
+      |> JsonNode.getOptJsonObject "followsCount"
       |> ValueOption.map(_.GetValue())
 
     let! postsCount =
       profile
-      |> JsonNode.tryGetProperty "postsCount"
+      |> JsonNode.getOptJsonObject "postsCount"
       |> ValueOption.map(_.GetValue())
 
     let! createdAt =
       profile
-      |> JsonNode.tryGetProperty "createdAt"
+      |> JsonNode.getOptJsonObject "createdAt"
       |> ValueOption.bind(fun v ->
         match DateTimeOffset.TryParse(v.GetValue<string>()) with
         | true, value -> ValueSome value
